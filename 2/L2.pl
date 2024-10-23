df8a9582cc91f1c26a26035dd05e8adac482a534
Lukas Pupelis <105020330+Lukas-Pupelis@users.noreply.github.com>
2024-10-23 21:06:16 +0300

    changed file name and changed names of variables

---
+++ 2/L2.pl
@@ -0,0 +1,51 @@
+% Autorius: Lukas Pupelis 2110612
+% 4 kursas 4 grupė
+% Variantai: 1.5, 5.3.
+
+% 1.5. - pravažiavus lygiai N tarpinių miestų.
+
+% Faktai apibrėžiantys kelius tarp miestų
+kelias(vilnius, ukmerge, 73). 
+kelias(vilnius, moletai, 64).
+kelias(moletai, utena, 35).
+kelias(moletai, anyksciai, 43).
+kelias(moletai, ukmerge, 44).
+kelias(utena, ukmerge, 63).
+kelias(utena, anyksciai, 39).
+kelias(utena, zarasai, 49).
+kelias(utena, rokiskis, 65).
+kelias(utena, kupiskis, 57).
+kelias(ukmerge, panevezys, 64).
+kelias(ukmerge, jonava, 37).
+kelias(anyksciai, rokiskis, 61).
+kelias(anyksciai, kupiskis, 47).
+kelias(jonava, kaunas, 32).
+kelias(panevezys, pakruojis, 50).
+kelias(panevezys, pasvalys, 40).
+
+/* 
+   Predikatas galima_nuvaziuoti(Miestas1, Miestas2) nustato, ar galima nuvažiuoti 
+   iš miesto Miestas1 į miestą Miestas2 per bet kokį skaičių tarpinių miestų.
+*/
+galima_nuvaziuoti(Miestas1, Miestas2) :-
+    kelias(Miestas1, Miestas2, _).
+
+galima_nuvaziuoti(Miestas1, Miestas2) :-
+    kelias(Miestas1, Z, _),
+    galima_nuvaziuoti(Z, Miestas2).
+
+/* 
+   Predikatas galima_nuvaziuoti(Miestas1, Miestas2, N) nustato, ar galima nuvažiuoti 
+   iš miesto Miestas1 į miestą Miestas2 praeinant būtent N tarpinių miestų.
+   
+   Čia N reiškia tik tarpinių miestų skaičių, tai yra, iš viso kelias susideda iš N+1 etapų.
+*/
+galima_nuvaziuoti(Miestas1, Miestas2, N) :-
+    N =:= 0,
+    kelias(Miestas1, Miestas2, _).
+
+galima_nuvaziuoti(Miestas1, Miestas2, N) :-
+    N > 0,
+    kelias(Miestas1, Z, _),
+    N1 is N - 1,
+    galima_nuvaziuoti(Z, Miestas2, N1).
