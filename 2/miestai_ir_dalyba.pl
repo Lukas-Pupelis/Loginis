b7719ba0e3534d6db725870880a8ec12ef5b265e
Lukas Pupelis <105020330+Lukas-Pupelis@users.noreply.github.com>
2024-10-23 20:52:18 +0300

    created condition file and initialised prolog file

---
+++ 2/miestai_ir_dalyba.pl
@@ -0,0 +1,23 @@
+% Autorius: Lukas Pupelis 2110612
+% 4 kursas 4 grupė
+% Variantai: 1.5, 5.3.
+
+% 1.5. - pravažiavus lygiai N tarpinių miestų.
+
+kelias(vilnius, ukmerge, 73).
+kelias(vilnius, moletai, 64).
+kelias(moletai, utena, 35).
+kelias(moletai, anyksciai, 43).
+kelias(moletai, ukmerge, 44).
+kelias(utena, ukmerge, 63).
+kelias(utena, anyksciai, 39).
+kelias(utena, zarasai, 49).
+kelias(utena, rokiskis, 65).
+kelias(utena, kupiskis, 57).
+kelias(ukmerge, panevezys, 64).
+kelias(ukmerge, jonava, 37).
+kelias(anyksciai, rokiskis, 61).
+kelias(anyksciai, kupiskis, 47).
+kelias(jonava, kaunas, 32).
+kelias(panevezys, pakruojis, 50).
+kelias(panevezys, pasvalys, 40).
